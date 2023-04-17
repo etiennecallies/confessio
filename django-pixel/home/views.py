3713d288368be6d50c7cd7d70f87385b561b6156
Etienne Callies <etienne.callies@ouihelp.fr>
2023-04-17 20:34:01 +0200

    ad map on home page

--- django-pixel/home/views.py
+++ django-pixel/home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import folium
 
 # Create your views here.
 
 def index(request):
 
+    # Create Map Object
+    m = folium.Map(location=[48.859, 2.342], zoom_start=13)
+
+    folium.Marker([48.85, 2.35], tooltip='Click for more', popup='FR').add_to(m)
+    # Get HTML Representation of Map Object
+    m = m._repr_html_()
+    context = {
+        'm': m,
+    }
+
     # Page from the theme 
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/index.html', context)
