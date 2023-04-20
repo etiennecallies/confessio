23f70fd26ffae22a921872c544c35599e696f1fd
Etienne Callies <etienne.callies@ouihelp.fr>
2023-04-20 17:49:51 +0200

    display churches on maps

--- home/views.py
+++ home/views.py
@@ -2,18 +2,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import folium
 
+from home.models import Parish
+
+
 # Create your views here.
 
+def get_latitude_longitude(point):
+    return [point.coords[1], point.coords[0]]
+
+
 def index(request):
+
+    # Load parishes
+    # TODO load parish and last scrappings at the same time
+    parishes = Parish.objects.all()
 
     # Create Map Object
     m = folium.Map(location=[48.859, 2.342], zoom_start=13)
 
-    folium.Marker([48.85, 2.35], tooltip='Click for more', popup='FR').add_to(m)
+    for parish in parishes:
+        for church in parish.churches.all():
+            popup_content = f'{church.name}'
+            folium.Marker(get_latitude_longitude(church.location),
+                          tooltip='Click for more',
+                          popup=popup_content,
+                          ).add_to(m)
+
     # Get HTML Representation of Map Object
-    m = m._repr_html_()
+    map_html = m._repr_html_()
+    print(map_html)
+
     context = {
-        'm': m,
+        'map_html': map_html,
+        'parishes': parishes
     }
 
     # Page from the theme 
