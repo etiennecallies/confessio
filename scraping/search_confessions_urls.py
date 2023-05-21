47e854304c2e1a46d5dbda62516a0fa3c356588d
Etienne Callies <etienne.callies@ouihelp.fr>
2023-05-21 18:31:52 +0200

    search link without test

---
+++ scraping/search_confessions_urls.py
@@ -0,0 +1,13 @@
+from scraping.utils.download_content import get_content_from_url
+from scraping.utils.extract_links import parse_content_links
+
+
+def fetch_list_of_links(home_url: str):
+    content = get_content_from_url(home_url)
+    parse_content_links(content, home_url)
+
+
+if __name__ == '__main__':
+    home_url = 'https://www.eglise-saintgermaindespres.fr/'
+    # home_url = 'https://www.saintjacquesduhautpas.com/'
+    fetch_list_of_links(home_url)
---
+++ scraping/utils/extract_links.py
@@ -0,0 +1,67 @@
+from urllib.parse import urlparse, ParseResult
+
+from bs4 import BeautifulSoup, SoupStrainer, Comment
+from bs4 import element as el
+
+from scraping.utils.extract_content import has_any_of_words
+
+CONFESSIONS_OR_SCHEDULES_MENTIONS = [
+    'confession',
+    'confessions',
+    'reconciliation',
+    'sacrement',
+    'sacrements',
+    'horaire',
+    'horaires',
+]
+
+
+def might_be_confession_link(path, text):
+    if has_any_of_words(path, CONFESSIONS_OR_SCHEDULES_MENTIONS) \
+            or has_any_of_words(text, CONFESSIONS_OR_SCHEDULES_MENTIONS):
+        return True
+
+    return False
+
+
+def is_internal_link(url: str, url_parsed: ParseResult, home_url_parsed: ParseResult):
+    if url.startswith('#'):
+        # link on same page
+        return False
+
+    if url_parsed.netloc != home_url_parsed.netloc:
+        # external link
+        return False
+
+    return True
+
+
+def get_links(element: el, home_url: str):
+    home_url_parsed = urlparse(home_url)
+
+    results = set()
+
+    for link in element:
+        if link.has_attr('href'):
+            full_url = link['href']
+            url_parsed = urlparse(full_url)
+
+            if not is_internal_link(full_url, url_parsed, home_url_parsed):
+                continue
+
+            # Extract link text
+            all_strings = link.find_all(text=lambda t: not isinstance(t, Comment),
+                                        recursive=True)
+            text = ' '.join(all_strings).rstrip()
+
+            if might_be_confession_link(url_parsed.path, text):
+                results.add(full_url)
+
+    return results
+
+
+def parse_content_links(content, home_url):
+    element = BeautifulSoup(content, 'html.parser', parse_only=SoupStrainer('a'))
+    links = get_links(element, home_url)
+    for link in links:
+        print(link)
