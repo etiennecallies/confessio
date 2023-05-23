a46fd957a284670e6ed70e9c5bd77412721bb2ab
Etienne Callies <etienne.callies@ouihelp.fr>
2023-05-23 20:35:19 +0200

    confession_html can be null

--- home/models.py
+++ home/models.py
@@ -73,6 +73,6 @@ class Page(TimeStampMixin):
 
 
 class Scraping(TimeStampMixin):
-    confession_html = models.TextField()
+    confession_html = models.TextField(null=True)
     nb_iterations = models.PositiveSmallIntegerField()
     page = models.ForeignKey('Page', on_delete=models.CASCADE, related_name='scrapings')
