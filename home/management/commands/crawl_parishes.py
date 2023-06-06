6d482ab0eca36e0fc0a6e0401e8a06d2e2cdac93
Etienne Callies <etienne.callies@ouihelp.fr>
2023-06-06 19:53:14 +0200

    can crawl a specific parish

--- home/management/commands/crawl_parishes.py
+++ home/management/commands/crawl_parishes.py
@@ -1,17 +1,25 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
-from home.models import Parish, Scraping, Page
-from scraping.search_confessions_paragraphs import get_fresh_confessions_part
+from home.models import Parish, Page
 from scraping.search_confessions_urls import search_for_confession_pages
 
 
 class Command(BaseCommand):
     help = "Launch the search of all urls with confessions hours, starting for home url"
 
+    def add_arguments(self, parser):
+        parser.add_argument('-n', '--name', help='name of parish to crawl')
+
     def handle(self, *args, **options):
-        parishes = Parish.objects.all()
+        if options['name']:
+            parishes = Parish.objects.filter(name__contains=options['name']).all()
+        else:
+            parishes = Parish.objects.all()
 
         for parish in parishes:
+            self.stdout.write(
+                self.style.HTTP_INFO(f'Starting crawling for parish {parish.name} {parish.uuid}')
+            )
             urls = search_for_confession_pages(parish.home_url)
             existing_urls = list(map(lambda p: p.url, parish.get_pages()))
 
