e034aae3c93e12fe0fa5ab4cd8015184d6f9c652
Etienne Callies <etienne.callies@ouihelp.fr>
2023-10-24 08:37:31 +0200

    rename paragraph -> extract

--- scraping/tests/tests_extract_content.py
+++ scraping/tests/tests_extract_content.py
@@ -32,9 +32,9 @@ class MyTestCase(unittest.TestCase):
         tests_dir = os.path.dirname(os.path.realpath(__file__))
         for file_name, page_type in self.get_paragraphs_fixtures():
             with self.subTest():
-                with open(f'{tests_dir}/fixtures/paragraphs/{file_name}.html') as f:
+                with open(f'{tests_dir}/fixtures/extract/{file_name}.html') as f:
                     lines = f.readlines()
-                with open(f'{tests_dir}/fixtures/paragraphs/{file_name}.txt') as f:
+                with open(f'{tests_dir}/fixtures/extract/{file_name}.txt') as f:
                     expected_lines = f.readlines()
                 content_html = ''.join(lines)
                 expected_confession_part = ''.join(expected_lines)
