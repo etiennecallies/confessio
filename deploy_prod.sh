17fde93edd2956588b2d0bb404521f5497fecfda
Etienne Callies <etienne.callies@ouihelp.fr>
2023-12-27 17:45:36 +0100

    forwrd ssh keys

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-. ./.env; ansible-playbook ansible/prod/deploy.yml -K -u ubuntu -i ansible/prod/hosts -vvv
+. ./.env; ANSIBLE_CONFIG=ansible/ansible.cfg ansible-playbook ansible/prod/deploy.yml -K -u ubuntu -i ansible/prod/hosts -vvv
