7187b7264f6b808b01aa270c4eae1d408cd985fd
Etienne Callies <etienne.callies@ouihelp.fr>
2024-01-16 22:17:23 +0100

    add additional public key

--- prod.sh
+++ prod.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# https://stackoverflow.com/questions/19331497/set-environment-variables-from-file-of-key-value-pairs/20909045#20909045
-unamestr=$(uname)
-if [ "$unamestr" = 'Linux' ]; then
-  export $(grep -v '^#' .env | xargs -d '\n')
-elif [ "$unamestr" = 'FreeBSD' ] || [ "$unamestr" = 'Darwin' ]; then
-  export $(grep -v '^#' .env | xargs -0)
-fi
-
 if [[ $# -eq 0 ]] ; then
     echo 'please specify install or deploy'
     exit 1
 fi
 
+source .env;
 ANSIBLE_CONFIG=ansible/ansible.cfg ansible-playbook ansible/prod/$1.yml -K -u ubuntu -i ansible/prod/hosts -vvv
