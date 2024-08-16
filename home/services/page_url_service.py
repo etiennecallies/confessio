adf3d75284eb37930ac0c3c46fe9f848f5a8e3a4
Etienne Callies <etienne.callies@ouihelp.fr>
2024-08-16 16:39:52 +0200

    strip pointer text

--- home/services/page_url_service.py
+++ home/services/page_url_service.py
@@ -3,14 +3,14 @@ from scraping.refine.refine_content import get_text_if_not_table
 
 
 def get_page_url_with_pointer(page: Page):
-    latest_scraping = page.get_latest_scraping()
+    latest_pruning = page.get_latest_pruning()
 
     pointer_text = None
-    if latest_scraping and latest_scraping.pruning and latest_scraping.pruning.pruned_html:
-        first_line = latest_scraping.pruning.pruned_html.split('<br>\n')[0]
+    if latest_pruning and latest_pruning.pruned_html:
+        first_line = latest_pruning.pruned_html.split('<br>\n')[0]
         pointer_text = get_text_if_not_table(first_line)
 
     if pointer_text is None:
         return page.url
 
-    return f'{page.url}#:~:text={pointer_text}'
+    return f'{page.url}#:~:text={pointer_text.strip()}'
