ac69ce03b51214f1523a2c7d994b4a2ca69207f3
Etienne Callies <etienne.callies@ouihelp.fr>
2024-11-04 12:46:29 +0100

    ignore media urls

--- scraping/download/download_content.py
+++ scraping/download/download_content.py
@@ -55,11 +55,14 @@ def get_content_length_by_streaming(url: str) -> int | None:
 
 
 def get_content_from_url(url):
-    for extension in ('.mp3', '.m4a'):
+    # ignore media urls
+    for extension in ('.mp3', '.m4a', '.wav', '.mp4', '.mov', '.avi', '.mkv', '.webm', '.flv',
+                      '.wmv', '.wma', '.flac'):
         if url.endswith(extension):
             print(f'ignoring {extension} url {url}')
             return None
 
+    # Handle heavy pdf files
     if url.endswith('.pdf'):
         content_length = get_content_length(url)
         if content_length is None:
