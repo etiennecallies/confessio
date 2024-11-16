5a5d57794ccba1aa307b2abd0a8da5f922439721
Etienne Callies <etienne.callies@ouihelp.fr>
2024-11-16 23:12:05 +0100

    add stats for unreliabity-reasons

--- home/management/commands/evaluate_quality.py
+++ home/management/commands/evaluate_quality.py
@@ -1,7 +1,7 @@
 from typing import Literal
 
 from home.management.abstract_command import AbstractCommand
-from home.models import Diocese, Page
+from home.models import Diocese, Page, Website
 from scraping.services.page_service import page_was_validated_at_first
 
 
@@ -9,7 +9,7 @@ class Command(AbstractCommand):
     help = "Evaluate pruning and parsing quality of pages of a given diocese"
 
     def add_arguments(self, parser):
-        parser.add_argument('messesinfo_network_id', type=str,
+        parser.add_argument('-d', '--messesinfo-network-id', type=str,
                             help='Two letters code of messesinfo network_id (diocese)')
 
     @staticmethod
@@ -28,15 +28,36 @@ class Command(AbstractCommand):
         return total_success / total_qualified if total_qualified > 0 else 0
 
     def handle(self, *args, **options):
-        messesinfo_network_id = options['messesinfo_network_id']
-        try:
-            diocese = Diocese.objects.get(messesinfo_network_id=messesinfo_network_id)
-        except Diocese.DoesNotExist:
-            self.error(f'No diocese for network_id {messesinfo_network_id}')
-            return
+        if not options['messesinfo_network_id']:
+            self.info(f'Getting all websites...')
+            websites = Website.objects.all()
+        else:
+            messesinfo_network_id = options['messesinfo_network_id']
+            try:
+                diocese = Diocese.objects.get(messesinfo_network_id=messesinfo_network_id)
+            except Diocese.DoesNotExist:
+                self.error(f'No diocese for network_id {messesinfo_network_id}')
+                return
 
-        self.info(f'Getting page of diocese {diocese.name}...')
-        pages = Page.objects.filter(website__parishes__diocese=diocese).distinct()
+            self.info(f'Getting websites of diocese {diocese.name}...')
+            websites = Website.objects.filter(parishes__diocese=diocese).all()
+
+        self.info(f'Successfully got {len(websites)} websites')
+        counter_by_unreliability_reason = {}
+        for website in websites:
+            if website.unreliability_reason:
+                counter_by_unreliability_reason[website.unreliability_reason] = \
+                    counter_by_unreliability_reason.get(website.unreliability_reason, 0) + 1
+
+        if not counter_by_unreliability_reason:
+            self.info('No unreliability reason for websites')
+
+        for unreliability_reason, counter in counter_by_unreliability_reason.items():
+            percentage = counter / len(websites) * 100
+            self.info(f'{unreliability_reason} : {counter} websites ({percentage:.2f} %)')
+
+        self.info(f'Getting all pages...')
+        pages = Page.objects.filter(website__in=websites).distinct()
         self.info(f'Successfully got {len(pages)} pages')
 
         pruning_resource: Literal['pruning', 'parsing'] = 'pruning'
