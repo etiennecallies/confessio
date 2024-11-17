934c0f06c9f5b44c758067276885fcc35cbea6f6
Etienne Callies <etienne.callies@ouihelp.fr>
2024-11-17 12:24:14 +0100

    do not use pruning any more

--- scraping/services/scrape_page_service.py
+++ scraping/services/scrape_page_service.py
@@ -6,33 +6,49 @@ from scraping.services.prune_scraping_service import prune_pruning, \
     create_pruning
 
 
+def is_extracted_html_list_identical_for_scraping(scraping: Scraping,
+                                                  extracted_html_list: list[str]) -> bool:
+    prunings = scraping.prunings.all()
+    if not prunings and not extracted_html_list:
+        return True
+
+    if not prunings or not extracted_html_list:
+        return False
+
+    return set(p.extracted_html for p in prunings) == set(extracted_html_list)
+
+
 def upsert_scraping(page: Page, extracted_html: Optional[str]) -> ():
+    extracted_html_list = [extracted_html] if extracted_html else []  # TODO adapt this
+
     # Compare result to last scraping
     scraping = page.get_latest_scraping()
     if (scraping is not None
-            and ((scraping.pruning is None
-                  and extracted_html is None)
-                 or (scraping.pruning is not None
-                     and scraping.pruning.extracted_html == extracted_html))):
+            and is_extracted_html_list_identical_for_scraping(scraping, extracted_html_list)):
         # If a scraping exists and is identical to last one
         scraping.nb_iterations += 1
         scraping.save()
 
-        if scraping.pruning is not None:
-            prune_pruning(scraping.pruning)
+        for pruning in scraping.prunings.all():
+            prune_pruning(pruning)
     else:
         if scraping is not None:
             # If a scraping exists and is different from last one, we delete it
             delete_scraping(scraping)
 
-        pruning = create_pruning(extracted_html)
+        prunings = []
+        for extracted_html_item in extracted_html_list:
+            prunings.append(create_pruning(extracted_html_item))
+
+        legacy_pruning = prunings[0] if prunings else None  # TODO remove this line
 
         scraping = Scraping(
             nb_iterations=1,
             page=page,
-            pruning=pruning,
+            pruning=legacy_pruning,  # TODO remove this line
         )
         scraping.save()
-        scraping.prunings.add(pruning)
 
-        prune_pruning(pruning)
+        for pruning in prunings:
+            scraping.prunings.add(pruning)
+            prune_pruning(pruning)
