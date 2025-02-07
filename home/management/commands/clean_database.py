27b473c1a22df5e9f3c12eef001adb7c0d27651d
Etienne Callies <etienne.callies@ouihelp.fr>
2025-02-07 13:33:36 +0100

    clean at one place

--- home/management/commands/clean_database.py
+++ home/management/commands/clean_database.py
@@ -5,13 +5,36 @@ from django.db.models import Q, Model
 from django.utils import timezone
 
 from home.management.abstract_command import AbstractCommand
-from home.models import Pruning, Sentence, Classifier
+from home.models import Pruning, Sentence, Classifier, Page
+from scraping.services.page_service import delete_page
+from scraping.services.parse_pruning_service import clean_parsing_moderations
+from scraping.services.prune_scraping_service import clean_pruning_moderations
+from scraping.services.scrape_page_service import delete_orphan_scrapings
 
 
 class Command(AbstractCommand):
     help = "Command to remove useless data from the database"
 
     def handle(self, *args, **options):
+        self.info(f'Starting removing pages of inactive websites')
+        delete_count = 0
+        for page in Page.objects.filter(website__is_active=False):
+            delete_page(page)
+            delete_count += 1
+        self.success(f'Successfully deleted {delete_count} pages')
+
+        self.info(f'Starting cleaning orphan scrapings')
+        delete_count = delete_orphan_scrapings()
+        self.success(f'Successfully cleaning {delete_count} orphan scrapings')
+
+        self.info(f'Starting cleaning pruning moderations')
+        delete_count = clean_pruning_moderations()
+        self.success(f'Successfully cleaning {delete_count} pruning moderations')
+
+        self.info(f'Starting cleaning parsing moderations')
+        delete_count = clean_parsing_moderations()
+        self.success(f'Successfully cleaning {delete_count} parsing moderations')
+
         # Prunings
         self.info(f'Starting removing orphan prunings')
         orphan_prunings = Pruning.objects.filter(scrapings__isnull=True,
--- home/management/commands/crawl_websites.py
+++ home/management/commands/crawl_websites.py
@@ -5,12 +5,8 @@ from django.db.models import F, Q
 from django.utils import timezone
 
 from home.management.abstract_command import AbstractCommand
-from home.models import Website, WebsiteModeration, Page
+from home.models import Website, WebsiteModeration
 from scraping.services.crawl_website_service import crawl_website
-from scraping.services.page_service import delete_page
-from scraping.services.parse_pruning_service import clean_parsing_moderations
-from scraping.services.prune_scraping_service import clean_pruning_moderations
-from scraping.services.scrape_page_service import delete_orphan_scrapings
 
 
 class Command(AbstractCommand):
@@ -25,22 +21,6 @@ class Command(AbstractCommand):
                             help='only websites that have not been crawled recently')
 
     def handle(self, *args, **options):
-        self.info(f'Starting removing pages of inactive websites')
-        delete_count = 0
-        for page in Page.objects.filter(website__is_active=False):
-            delete_page(page)
-            delete_count += 1
-        self.success(f'Successfully deleted {delete_count} pages')
-        self.info(f'Starting cleaning orphan scrapings')
-        delete_count = delete_orphan_scrapings()
-        self.success(f'Successfully cleaning {delete_count} orphan scrapings')
-        self.info(f'Starting cleaning pruning moderations')
-        delete_count = clean_pruning_moderations()
-        self.success(f'Successfully cleaning {delete_count} pruning moderations')
-        self.info(f'Starting cleaning parsing moderations')
-        delete_count = clean_parsing_moderations()
-        self.success(f'Successfully cleaning {delete_count} parsing moderations')
-
         if options['name']:
             websites = Website.objects.filter(is_active=True, name__contains=options['name']).all()
         elif options['in_error']:
