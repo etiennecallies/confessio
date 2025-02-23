b70fe2e2c6095b99d2bbda6fc8fd51066f1e2733
Etienne Callies <etienne.callies@ouihelp.fr>
2025-02-23 23:47:40 +0100

    fix leap year

--- home/utils/date_utils.py
+++ home/utils/date_utils.py
@@ -90,17 +90,30 @@ def format_datetime_with_locale(dt: datetime, dt_format: str, locale_name: str) 
 # OTHER #
 #########
 
+def is_29th_february(month: int, day: int) -> bool:
+    return month == 2 and day == 29
+
+
+def is_leap_year(year: int) -> bool:
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+
+
 def guess_year_from_weekday(default_year: int, month: int, day: int, weekday_iso8601: int) -> int:
-    start_date = datetime(default_year, month, day)
+    start_date = datetime(2000, month, day)  # 2000 is a leap year
     weekday_python = weekday_iso8601 - 1
 
     for year in [default_year, default_year + 1]:
+        if is_29th_february(month, day) and not is_leap_year(year):
+            continue
         if start_date.replace(year=year).weekday() == weekday_python:
             return year
 
     max_count = 28  # 28 years is the maximum difference between two years with the same weekday
     for year_count in range(1, max_count + 1):
         year = default_year - year_count
+
+        if is_29th_february(month, day) and not is_leap_year(year):
+            continue
         if start_date.replace(year=year).weekday() == weekday_python:
             return year
 
