e7f21f41a62bc0c264794ccb6685f334b827a6b7
Etienne Callies <etienne.callies@ouihelp.fr>
2025-02-26 14:57:38 +0100

    click events

--- static/js/website_card0.js
+++ static/js/website_card0.js
@@ -19,28 +19,62 @@ $(document).ready(function () {
     $(this).children('.expandable-item').toggle();
   });
 
-  $('.collapsable-header').click(function() {
-    $(this).find('.toggle-symbol').toggleClass('collapsed-symbol expanded-symbol');
+  // Listen for Bootstrap collapse events instead of clicks
+  $('.collapse').on('shown.bs.collapse hidden.bs.collapse', function() {
+    // Find the associated header and toggle the symbol
+    $(this).prev('.collapsable-header').find('.toggle-symbol')
+      .toggleClass('collapsed-symbol expanded-symbol');
   });
 });
 
 /**
  * Activate tooltips.
  */
-$(window).on("load", function () {
+$(document).ready(function () {
   $('[data-toggle="tooltip"]').each(function() {
+      // use data-html="true" to enable html in tooltips
       $(this).tooltip({html: $(this).data('html') || false});
   });
 });
 
 /**
- * Open tab of given id.
+ * Navigate to the church element.
  */
-function openTab(tabId) {
-  // Open the tab
-  let $tabAnchor = $('.nav-tabs a[href="#' + tabId + '"]');
-  if ($tabAnchor.length === 0) {
-    return
+function goToChurch(websiteUuid, churchUuid, isChurchExplicitlyOther) {
+  // Uncollapse the churches section.
+  $('#churches-'+websiteUuid).collapse('show');
+
+  let $churchEl = churchUuid ? $('#church-'+churchUuid) : $('#nochurch-'+websiteUuid+'-'+isChurchExplicitlyOther);
+  // Navigate to the church element
+  if ($churchEl.length > 0) {
+    $churchEl[0].scrollIntoView();
+    animateElement($churchEl.first());
+  } else {
+      console.error('No church found for websiteUuid='+websiteUuid+' and churchUuid='+churchUuid);
   }
-  $tabAnchor.trigger('click');
+}
+
+/**
+ * Navigate to the parsing element.
+ */
+function goToParsing(websiteUuid, parsingUuid) {
+    // Uncollapse the parsing section.
+    $('#sources-'+websiteUuid).collapse('show');
+
+    let $parsingEl = $('#parsing-'+websiteUuid+'-'+parsingUuid);
+    // Navigate to the parsing element
+    if ($parsingEl.length > 0) {
+        $parsingEl[0].scrollIntoView();
+        animateElement($parsingEl.first());
+    } else {
+        console.error('No parsing found for websiteUuid='+websiteUuid+' and parsingUuid='+parsingUuid);
+    }
+}
+
+function animateElement($element) {
+    $element.css("background-color", "#f1f9fd");
+
+    setTimeout(() => {
+      $element.css("background-color", "");
+    }, 1000); // Reset after 1000ms
 }
