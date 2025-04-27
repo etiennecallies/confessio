04253b0f178781403290f26500b4c40d1d5dccfc
Etienne Callies <etienne.callies@ouihelp.fr>
2025-04-27 12:43:35 +0200

    fix command duplicate

--- home/management/abstract_command.py
+++ home/management/abstract_command.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.core.management import BaseCommand
 
-from home.utils.log_utils import info
+from home.utils.log_utils import add_line_to_buffer
 
 
 class AbstractCommand(BaseCommand):
@@ -18,7 +18,7 @@ class AbstractCommand(BaseCommand):
     def log(self, m, modifier):
         message_to_log = f'{datetime.now()} {m}'
         self.stdout.write(modifier(message_to_log))
-        info(m)
+        add_line_to_buffer(message_to_log)
 
     def success(self, m):
         self.log(m, self.style.SUCCESS)
--- home/utils/log_utils.py
+++ home/utils/log_utils.py
@@ -6,13 +6,9 @@ buffer: ContextVar[str | None] = ContextVar('buffer', default=None)
 
 
 def info(message: any = ''):
-    global buffer
     log_to_print = f'{datetime.now()} {str(message)}'
     print(log_to_print)
-
-    buffer_value = buffer.get()
-    if buffer_value is not None:
-        buffer.set(buffer_value + f'{log_to_print}\n')
+    add_line_to_buffer(log_to_print)
 
 
 def start_log_buffer():
@@ -23,6 +19,14 @@ def start_log_buffer():
 def get_log_buffer() -> str:
     global buffer
     return buffer.get()
+
+
+def add_line_to_buffer(line: str):
+    global buffer
+
+    buffer_value = buffer.get()
+    if buffer_value is not None:
+        buffer.set(buffer_value + f'{line}\n')
 
 
 def append_to_buffer(content: str):
