8982d8f41ba466ad221cd1204545cf421f849129
Etienne Callies <etienne.callies@ouihelp.fr>
2025-05-03 10:11:17 +0200

    fix site web

--- home/views/index_views.py
+++ home/views/index_views.py
@@ -283,7 +283,7 @@ Merci de nous remonter d'éventuelles erreurs. Bonne confession !"""
         if not day_filter and hour_min is None and hour_max is None:
             too_many_results = False
             welcome_message = """👋 Bienvenue ! Confessio affiche les horaires des confessions
-indiqués sur les sites webs des paroisses, et sera national en décembre 2025. N'hésitez pas à
+indiqués sur les sites web des paroisses, et sera national en décembre 2025. N'hésitez pas à
 remonter d'éventuelles erreurs. Merci et bonne confession !"""
             display_quick_search_cities = True
 
