040d0a94e25cf4d38f43d8690380dbcc22c03259
Etienne Callies <etienne.callies@ouihelp.fr>
2025-07-25 10:57:18 +0200

    limit on number of uploaded images

--- home/services/upload_image_service.py
+++ home/services/upload_image_service.py
@@ -1,6 +1,9 @@
+from datetime import datetime, timedelta
+
 import boto3
 from django.conf import settings
 from django.urls import reverse
+from django.utils.timezone import make_aware
 
 from home.models import Website, Image
 from home.services.admin_email_service import send_email_to_admin
@@ -8,6 +11,7 @@ from home.utils.web_utils import get_user_user_agent_and_ip
 
 
 IMAGE_SIZE_LIMIT = 10 * 1024 * 1024  # 10MB limit for uploaded images
+MAX_IMAGES_IN_30_DAYS = 2000  # Maximum number of images allowed in the last 30 days
 
 
 def find_error_in_document_to_upload(document) -> str | None:
@@ -23,6 +27,12 @@ def find_error_in_document_to_upload(document) -> str | None:
 
 
 def upload_image(document, website: Website, request) -> tuple[bool, str | None]:
+    if too_many_recent_images():
+        subject = 'Too many images uploaded recently'
+        send_email_to_admin(subject, subject)
+
+        return False, "Trop d'images ont été téléchargées récemment. Veuillez réessayer plus tard."
+
     # Generate unique filename
     image_name = document.name.replace(' ', '_').replace('/', '_')
     comment = request.POST.get('comment', None)
@@ -87,3 +97,10 @@ def get_image_public_url(image: Image) -> str:
     """Get the public URL of an image stored in S3."""
     host = f"{settings.AWS_STORAGE_BUCKET_NAME}.s3.{settings.AWS_S3_REGION_NAME}.amazonaws.com"
     return f"https://{host}/{image.uuid}/{image.name}"
+
+
+def too_many_recent_images() -> bool:
+    last_30_days = datetime.now() - timedelta(days=30)
+    recent_images_count = Image.objects.filter(created_at__gt=make_aware(last_30_days)).count()
+
+    return recent_images_count > MAX_IMAGES_IN_30_DAYS
