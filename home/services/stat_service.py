d806e0ca5b86027f8b25226188fc2765efd58862
Etienne Callies <etienne.callies@ouihelp.fr>
2025-09-18 17:06:41 +0200

    fix query too long

--- home/services/stat_service.py
+++ home/services/stat_service.py
@@ -6,7 +6,7 @@ def new_search_hit(request, nb_websites):
     user, user_agent, ip_address_hash = get_user_user_agent_and_ip(request)
 
     search_hit = SearchHit(
-        query=request.get_full_path(),
+        query=request.get_full_path()[255:],
         nb_websites=nb_websites,
         user=user,
         user_agent=user_agent,
