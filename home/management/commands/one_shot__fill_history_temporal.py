69b3c01cc037aba7410d5dc4422f2bd35442bb45
Etienne Callies <etienne.callies@ouihelp.fr>
2025-09-19 21:50:20 +0200

    one_shot__fill_history_temporal

---
+++ home/management/commands/one_shot__fill_history_temporal.py
@@ -0,0 +1,21 @@
+from home.management.abstract_command import AbstractCommand
+from home.models import Sentence
+
+
+class Command(AbstractCommand):
+    help = "One shot command to fill history temporal."
+
+    def handle(self, *args, **options):
+        counter = 0
+        for sentence in Sentence.objects.all():
+            assert sentence.ml_temporal is not None
+            Sentence.history.model.objects.filter(
+                uuid=sentence.uuid,
+                ml_temporal__isnull=True
+            ).update(ml_temporal=sentence.ml_temporal)
+
+            counter += 1
+            if counter % 100 == 0:
+                self.stdout.write(f'Processed {counter} sentences...')
+
+        self.success(f'Successfully fill {counter} history temporal.')
