dd7b38059d618828b29268088b5f96b1e490e5a2
Etienne Callies <etienne.callies@ouihelp.fr>
2025-09-22 17:51:07 +0200

    adding ram debug

--- scraping/services/scrape_page_service.py
+++ scraping/services/scrape_page_service.py
@@ -1,6 +1,7 @@
 from home.models import Page, Scraping
 from scraping.services.page_service import delete_scraping, check_for_orphan_prunings
 from scraping.services.prune_scraping_service import create_pruning, prune_pruning
+from scraping.utils.ram_utils import print_memory_usage
 
 
 def is_extracted_html_list_identical_for_scraping(scraping: Scraping,
@@ -18,6 +19,7 @@ def is_extracted_html_list_identical_for_scraping(scraping: Scraping,
 def upsert_extracted_html_list(page: Page, extracted_html_list: list[str]):
     prunings_to_prune = []
     old_prunings = []
+    print_memory_usage('begin upsert_extracted_html_list')
 
     # Compare result to last scraping
     if (page.has_been_scraped()
@@ -27,11 +29,15 @@ def upsert_extracted_html_list(page: Page, extracted_html_list: list[str]):
         page.scraping.nb_iterations += 1
         page.scraping.save()
 
+        print_memory_usage('before appending prunings')
+
         for pruning in page.scraping.prunings.all():
             prunings_to_prune.append(pruning)
     else:
         for extracted_html_item in extracted_html_list:
             prunings_to_prune.append(create_pruning(extracted_html_item))
+
+        print_memory_usage('after create_pruning')
 
         if page.has_been_scraped():
             old_prunings = list(page.scraping.prunings.all())
@@ -43,14 +49,22 @@ def upsert_extracted_html_list(page: Page, extracted_html_list: list[str]):
         )
         scraping.save()
 
+        print_memory_usage('after scraping.save()')
+
         for pruning in prunings_to_prune:
             scraping.prunings.add(pruning)
+
+    print_memory_usage('before prune_pruning')
 
     for pruning in prunings_to_prune:
         prune_pruning(pruning)
 
+    print_memory_usage('before check_for_orphan_prunings')
+
     if old_prunings:
         check_for_orphan_prunings(old_prunings, page.website)
+
+    print_memory_usage('end upsert_extracted_html_list')
 
 
 def delete_orphan_scrapings() -> int:
