16ddad720e4376efc7cdd3b1fbabf39121d0253b
Etienne Callies <etienne.callies@ouihelp.fr>
2025-11-03 13:27:45 +0100

    more school holidays

--- scraping/parse/holidays.py
+++ scraping/parse/holidays.py
@@ -4,6 +4,8 @@ from datetime import date, timedelta, datetime
 from enum import Enum
 from typing import List, Dict
 
+import httpx
+
 from home.utils.date_utils import get_current_day
 
 
@@ -196,6 +198,8 @@ def print_holidays_for_zone(ics_content, default_zones: list | None = None):
 
     for item in parse_ics(ics_content, default_zones):
         year = item['start_date'].year
+        if year < 2024:
+            continue
         for zone in item['zones']:
             holiday_by_zone[zone].setdefault(year, []).append((item['start_date'],
                                                                item['end_date']))
@@ -214,321 +218,16 @@ def print_holidays_for_zone(ics_content, default_zones: list | None = None):
     print(pprint.pformat(holiday_by_zone))
 
 
+def get_ics_content_from_url(ics_url: str) -> str:
+    response = httpx.get(ics_url)
+    return response.text
+
+
 def generate_and_print_holiday_by_zone():
     # https://www.data.gouv.fr/fr/datasets/5889d03ea3a72974cbf0d5b0/#/resources
-    ics_content = """
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260704
-    DTEND;VALUE=DATE:20260704
-    SUMMARY:Début des Vacances d'Été - Zones A/B/C
-    UID:20250114T124833Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Début des Vacances d'Été - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260514
-    DTEND;VALUE=DATE:20260518
-    SUMMARY:Pont de l'Ascension - Zones A/B/C
-    UID:20250114T124834Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Pont de l'Ascension - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260427
-    DTEND;VALUE=DATE:20260504
-    SUMMARY:Vacances de Printemps - Zone C
-    UID:20250114T124835Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone C
-    DESCRIPTION:Vacances de Printemps - Zone C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260420
-    DTEND;VALUE=DATE:20260427
-    SUMMARY:Vacances de Printemps - Zones B/C
-    UID:20250114T124836Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones B/C
-    DESCRIPTION:Vacances de Printemps - Zones B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260418
-    DTEND;VALUE=DATE:20260420
-    SUMMARY:Vacances de Printemps - Zones A/B/C
-    UID:20250114T124837Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances de Printemps - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260411
-    DTEND;VALUE=DATE:20260418
-    SUMMARY:Vacances de Printemps - Zones A/B
-    UID:20250114T124838Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B
-    DESCRIPTION:Vacances de Printemps - Zones A/B
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260404
-    DTEND;VALUE=DATE:20260411
-    SUMMARY:Vacances de Printemps - Zone A
-    UID:20250114T124839Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone A
-    DESCRIPTION:Vacances de Printemps - Zone A
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260302
-    DTEND;VALUE=DATE:20260309
-    SUMMARY:Vacances d'Hiver - Zone C
-    UID:20250114T124840Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone C
-    DESCRIPTION:Vacances d'Hiver - Zone C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260223
-    DTEND;VALUE=DATE:20260302
-    SUMMARY:Vacances d'Hiver - Zones B/C
-    UID:20250114T124841Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones B/C
-    DESCRIPTION:Vacances d'Hiver - Zones B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260221
-    DTEND;VALUE=DATE:20260223
-    SUMMARY:Vacances d'Hiver - Zones A/B/C
-    UID:20250114T124842Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances d'Hiver - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260214
-    DTEND;VALUE=DATE:20260221
-    SUMMARY:Vacances d'Hiver - Zones A/B
-    UID:20250114T124843Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B
-    DESCRIPTION:Vacances d'Hiver - Zones A/B
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20260207
-    DTEND;VALUE=DATE:20260214
-    SUMMARY:Vacances d'Hiver - Zone A
-    UID:20250114T124844Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone A
-    DESCRIPTION:Vacances d'Hiver - Zone A
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20251220
-    DTEND;VALUE=DATE:20260105
-    SUMMARY:Vacances de Noël - Zones A/B/C
-    UID:20250114T124845Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances de Noël - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20251018
-    DTEND;VALUE=DATE:20251103
-    SUMMARY:Vacances de la Toussaint - Zones A/B/C
-    UID:20250114T124846Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances de la Toussaint - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250829
-    DTEND;VALUE=DATE:20250901
-    SUMMARY:Vacances d'Été(prérentrée Enseignants) - Zones A/B/C
-    UID:20250114T124847Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances d'Été(prérentrée Enseignants) - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250705
-    DTEND;VALUE=DATE:20250901
-    SUMMARY:Vacances d'Été - Zones A/B/C
-    UID:20250114T124848Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C
-    DESCRIPTION:Vacances d'Été - Zones A/B/C
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250705
-    DTEND;VALUE=DATE:20250705
-    SUMMARY:Début des Vacances d'Été - Corse
-    UID:20250114T124849Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Corse
-    DESCRIPTION:Début des Vacances d'Été - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250609
-    DTEND;VALUE=DATE:20250609
-    SUMMARY:Lundi de Pentecôte - Corse
-    UID:20250114T124850Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Corse
-    DESCRIPTION:Lundi de Pentecôte - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250529
-    DTEND;VALUE=DATE:20250602
-    SUMMARY:Pont de l'Ascension - Zones A/B/C - Corse
-    UID:20250114T124851Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C - Corse
-    DESCRIPTION:Pont de l'Ascension - Zones A/B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250428
-    DTEND;VALUE=DATE:20250505
-    SUMMARY:Vacances de Printemps - Zone A
-    UID:20250114T124852Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone A
-    DESCRIPTION:Vacances de Printemps - Zone A
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250422
-    DTEND;VALUE=DATE:20250428
-    SUMMARY:Vacances de Printemps - Zones A/C - Corse
-    UID:20250114T124853Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/C - Corse
-    DESCRIPTION:Vacances de Printemps - Zones A/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250419
-    DTEND;VALUE=DATE:20250422
-    SUMMARY:Vacances de Printemps - Zones A/B/C - Corse
-    UID:20250114T124854Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C - Corse
-    DESCRIPTION:Vacances de Printemps - Zones A/B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250412
-    DTEND;VALUE=DATE:20250419
-    SUMMARY:Vacances de Printemps - Zones B/C - Corse
-    UID:20250114T124855Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones B/C - Corse
-    DESCRIPTION:Vacances de Printemps - Zones B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250405
-    DTEND;VALUE=DATE:20250412
-    SUMMARY:Vacances de Printemps - Zone B
-    UID:20250114T124856Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone B
-    DESCRIPTION:Vacances de Printemps - Zone B
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250303
-    DTEND;VALUE=DATE:20250310
-    SUMMARY:Vacances d'Hiver - Zone A
-    UID:20250114T124857Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone A
-    DESCRIPTION:Vacances d'Hiver - Zone A
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250224
-    DTEND;VALUE=DATE:20250303
-    SUMMARY:Vacances d'Hiver - Zones A/C - Corse
-    UID:20250114T124858Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/C - Corse
-    DESCRIPTION:Vacances d'Hiver - Zones A/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250222
-    DTEND;VALUE=DATE:20250224
-    SUMMARY:Vacances d'Hiver - Zones A/B/C - Corse
-    UID:20250114T124859Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C - Corse
-    DESCRIPTION:Vacances d'Hiver - Zones A/B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250215
-    DTEND;VALUE=DATE:20250222
-    SUMMARY:Vacances d'Hiver - Zones B/C - Corse
-    UID:20250114T124900Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones B/C - Corse
-    DESCRIPTION:Vacances d'Hiver - Zones B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20250208
-    DTEND;VALUE=DATE:20250215
-    SUMMARY:Vacances d'Hiver - Zone B
-    UID:20250114T124901Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zone B
-    DESCRIPTION:Vacances d'Hiver - Zone B
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20241221
-    DTEND;VALUE=DATE:20250106
-    SUMMARY:Vacances de Noël - Zones A/B/C - Corse
-    UID:20250114T124902Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C - Corse
-    DESCRIPTION:Vacances de Noël - Zones A/B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    DTSTAMP:20250114T124531Z
-    DTSTART;VALUE=DATE:20241019
-    DTEND;VALUE=DATE:20241104
-    SUMMARY:Vacances de la Toussaint - Zones A/B/C - Corse
-    UID:20250114T124903Z-Zone-A-B-C-Corse@data.education.gouv.fr
-    LOCATION:Zones A/B/C - Corse
-    DESCRIPTION:Vacances de la Toussaint - Zones A/B/C - Corse
-    TRANSP:TRANSPARENT
-    END:VEVENT
-    BEGIN:VEVENT
-    """
+    ics_url = ('https://fr.ftp.opendatasoft.com/openscol/fr-en-calendrier-scolaire/'
+               'Zone-A-B-C-Corse.ics')
+    ics_content = get_ics_content_from_url(ics_url)
 
     print_holidays_for_zone(ics_content)
 
@@ -1221,7 +920,10 @@ HOLIDAY_BY_ZONE = {
                        (date(2025, 12, 20), date(2026, 1, 5))],
                 2026: [(date(2026, 2, 14), date(2026, 3, 2)),
                        (date(2026, 4, 11), date(2026, 4, 27))]},
-    'fr_zone_a': {2024: [(date(2024, 10, 19), date(2024, 11, 4)),
+    'fr_zone_a': {2024: [(date(2024, 2, 17), date(2024, 3, 4)),
+                         (date(2024, 4, 13), date(2024, 4, 29)),
+                         (date(2024, 7, 6), date(2024, 9, 2)),
+                         (date(2024, 10, 19), date(2024, 11, 4)),
                          (date(2024, 12, 21), date(2025, 1, 6))],
                   2025: [(date(2025, 2, 22), date(2025, 3, 10)),
                          (date(2025, 4, 19), date(2025, 5, 5)),
@@ -1229,8 +931,16 @@ HOLIDAY_BY_ZONE = {
                          (date(2025, 10, 18), date(2025, 11, 3)),
                          (date(2025, 12, 20), date(2026, 1, 5))],
                   2026: [(date(2026, 2, 7), date(2026, 2, 23)),
-                         (date(2026, 4, 4), date(2026, 4, 20))]},
-    'fr_zone_b': {2024: [(date(2024, 10, 19), date(2024, 11, 4)),
+                         (date(2026, 4, 4), date(2026, 4, 20)),
+                         (date(2026, 7, 4), date(2026, 9, 1)),
+                         (date(2026, 10, 17), date(2026, 11, 2)),
+                         (date(2026, 12, 19), date(2027, 1, 4))],
+                  2027: [(date(2027, 2, 13), date(2027, 3, 1)),
+                         (date(2027, 4, 10), date(2027, 4, 26))]},
+    'fr_zone_b': {2024: [(date(2024, 2, 24), date(2024, 3, 11)),
+                         (date(2024, 4, 20), date(2024, 5, 6)),
+                         (date(2024, 7, 6), date(2024, 9, 2)),
+                         (date(2024, 10, 19), date(2024, 11, 4)),
                          (date(2024, 12, 21), date(2025, 1, 6))],
                   2025: [(date(2025, 2, 8), date(2025, 2, 24)),
                          (date(2025, 4, 5), date(2025, 4, 22)),
@@ -1238,9 +948,16 @@ HOLIDAY_BY_ZONE = {
                          (date(2025, 10, 18), date(2025, 11, 3)),
                          (date(2025, 12, 20), date(2026, 1, 5))],
                   2026: [(date(2026, 2, 14), date(2026, 3, 2)),
-                         (date(2026, 4, 11),
-                          date(2026, 4, 27))]},
-    'fr_zone_c': {2024: [(date(2024, 10, 19), date(2024, 11, 4)),
+                         (date(2026, 4, 11), date(2026, 4, 27)),
+                         (date(2026, 7, 4), date(2026, 9, 1)),
+                         (date(2026, 10, 17), date(2026, 11, 2)),
+                         (date(2026, 12, 19), date(2027, 1, 4))],
+                  2027: [(date(2027, 2, 20), date(2027, 3, 8)),
+                         (date(2027, 4, 17), date(2027, 5, 3))]},
+    'fr_zone_c': {2024: [(date(2024, 2, 10), date(2024, 2, 26)),
+                         (date(2024, 4, 6), date(2024, 4, 22)),
+                         (date(2024, 7, 6), date(2024, 9, 2)),
+                         (date(2024, 10, 19), date(2024, 11, 4)),
                          (date(2024, 12, 21), date(2025, 1, 6))],
                   2025: [(date(2025, 2, 15), date(2025, 3, 3)),
                          (date(2025, 4, 12), date(2025, 4, 28)),
@@ -1248,7 +965,12 @@ HOLIDAY_BY_ZONE = {
                          (date(2025, 10, 18), date(2025, 11, 3)),
                          (date(2025, 12, 20), date(2026, 1, 5))],
                   2026: [(date(2026, 2, 21), date(2026, 3, 9)),
-                         (date(2026, 4, 18), date(2026, 5, 4))]},
+                         (date(2026, 4, 18), date(2026, 5, 4)),
+                         (date(2026, 7, 4), date(2026, 9, 1)),
+                         (date(2026, 10, 17), date(2026, 11, 2)),
+                         (date(2026, 12, 19), date(2027, 1, 4))],
+                  2027: [(date(2027, 2, 6), date(2027, 2, 22)),
+                         (date(2027, 4, 3), date(2027, 4, 19))]},
     'guadeloupe': {2025: [(date(2025, 2, 22), date(2025, 3, 10)),
                           (date(2025, 4, 17), date(2025, 5, 5)),
                           (date(2025, 7, 5), date(2025, 9, 1)),
@@ -1280,7 +1002,7 @@ def check_holiday_by_zone() -> bool:
 
 
 if __name__ == '__main__':
-    # generate_and_print_holiday_by_zone()
+    generate_and_print_holiday_by_zone()
     # generate_and_print_corsica_holiday()
     # generate_and_print_guadeloupe_holiday()
-    generate_and_print_martinique_holiday()
+    # generate_and_print_martinique_holiday()
