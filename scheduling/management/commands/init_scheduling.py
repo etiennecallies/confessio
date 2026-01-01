7a86b17765fad42106de68ab267c4e952309eebb
Etienne Callies <etienne.callies@ouihelp.fr>
2026-01-01 18:17:42 +0100

    add cron for init_scheduling

--- scheduling/management/commands/init_scheduling.py
+++ scheduling/management/commands/init_scheduling.py
@@ -4,15 +4,20 @@ from scheduling.process import init_scheduling
 
 
 class Command(AbstractCommand):
-    help = "Init scheduling for a website."
+    help = "Init scheduling for one or all website(s)."
 
     def add_arguments(self, parser):
-        parser.add_argument('-n', '--name', help='name of website to crawl', required=True)
+        parser.add_argument('-n', '--name', help='name of website to crawl')
 
     def handle(self, *args, **options):
-        self.info(f'Starting init scheduling for websites matching name: {options["name"]}')
-        websites = Website.objects.filter(is_active=True, name__contains=options['name']).all()
+        if options["name"]:
+            self.info(f'Starting init scheduling for websites matching name: {options["name"]}')
+            websites = Website.objects.filter(is_active=True, name__contains=options['name']).all()
+        else:
+            self.info('Starting init scheduling for all active websites')
+            websites = Website.objects.filter(is_active=True).all()
+
         for website in websites:
             self.info(f'Processing website: {website.name} (ID: {website.uuid})')
             init_scheduling(website)
-        self.success(f'Finished init scheduling for websites matching name: {options["name"]}')
+        self.success(f'Finished init scheduling for {len(websites)} website(s)')
