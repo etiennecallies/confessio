9ee212778cd041f9e0b1dfee7d502b2ca9ac9b44
Etienne Callies <etienne.callies@ouihelp.fr>
2026-01-19 22:40:34 +0100

    one_shot__clean_scrapings

---
+++ home/management/commands/one_shot__clean_scrapings.py
@@ -0,0 +1,19 @@
+from home.management.abstract_command import AbstractCommand
+from home.models import Scraping
+from scraping.services.scraping_service import delete_scraping
+
+
+class Command(AbstractCommand):
+    help = "One shot command to clean scrapings."
+
+    def handle(self, *args, **options):
+        self.info('Starting one shot command to clean scrapings...')
+
+        counter = 0
+        for scraping in Scraping.objects.filter(website__isnull=True).all():
+            delete_scraping(scraping)
+            counter += 1
+            if counter % 10 == 0:
+                self.info(f'Cleaned {counter} scrapings so far...')
+
+        self.success(f'Successfully cleaned {counter} scrapings.')
