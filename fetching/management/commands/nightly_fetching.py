15717fb70c4cb46ada905f43acce55efa97a4f52
Etienne Callies <etienne.callies@ouihelp.fr>
2026-01-26 19:07:54 +0100

    add heartbeat for nightly fetching

--- fetching/management/commands/nightly_fetching.py
+++ fetching/management/commands/nightly_fetching.py
@@ -1,5 +1,6 @@
 from fetching.process import nightly_synchronization
 from home.management.abstract_command import AbstractCommand
+from home.management.utils.heartbeat_utils import ping_heartbeat
 
 
 class Command(AbstractCommand):
@@ -8,4 +9,5 @@ class Command(AbstractCommand):
     def handle(self, *args, **options):
         self.info(f'Starting nightly fetching and processing')
         nightly_synchronization()
+        ping_heartbeat("HEARTBEAT_NIGHTLY_FETCHING_URL")
         self.success(f'Finished nightly fetching and processing')
