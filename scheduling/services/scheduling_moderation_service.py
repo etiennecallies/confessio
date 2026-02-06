5d46c5248ef1b24622ad8d140bcd594e8882f9ee
Etienne Callies <etienne.callies@ouihelp.fr>
2026-02-06 13:09:34 +0100

    moderation validated for scheduling

--- scheduling/services/scheduling_moderation_service.py
+++ scheduling/services/scheduling_moderation_service.py
@@ -1,33 +1,50 @@
+from django.db.models.functions import Now
+
+from crawling.models import CrawlingModeration
 from registry.models import Website
 from scheduling.models import Scheduling
 from scheduling.models.scheduling_moderation_models import SchedulingModeration
 from scheduling.services.scheduling_service import get_scheduling_sources
 
 
-def upsert_scheduling_moderation(website: Website, category: SchedulingModeration.Category):
+def upsert_scheduling_moderation(website: Website, category: SchedulingModeration.Category,
+                                 moderation_validated: bool):
     try:
         moderation = SchedulingModeration.objects.get(website=website)
         if moderation.category != category:
             moderation.category = category
+            moderation.validated_at = Now() if moderation_validated else None
+            moderation.validated_by = None
             moderation.save()
     except SchedulingModeration.DoesNotExist:
         moderation = SchedulingModeration(
             website=website, category=category,
             diocese=website.get_diocese(),
+            validated_at=Now() if moderation_validated else None,
         )
         moderation.save()
 
 
-def get_scheduling_moderation_category(scheduling: Scheduling) -> SchedulingModeration.Category:
+def has_crawling_moderation_been_validated(scheduling: Scheduling) -> bool:
+    return CrawlingModeration.objects.filter(
+        website=scheduling.website,
+        category__in=[CrawlingModeration.Category.NO_RESPONSE, CrawlingModeration.Category.NO_PAGE],
+        validated_at__isnull=False
+    ).exists()
+
+
+def get_scheduling_moderation_category(scheduling: Scheduling
+                                       ) -> tuple[SchedulingModeration.Category, bool]:
     scheduling_sources = get_scheduling_sources(scheduling)
     if not scheduling_sources.parsings and not scheduling_sources.oclocher_schedules:
-        return SchedulingModeration.Category.NO_SOURCE
+        moderation_validated = has_crawling_moderation_been_validated(scheduling)
+        return SchedulingModeration.Category.NO_SOURCE, moderation_validated
 
     # TODO add more catagories here
 
-    return SchedulingModeration.Category.OK
+    return SchedulingModeration.Category.OK, False
 
 
 def add_necessary_scheduling_moderation(scheduling: Scheduling):
-    category = get_scheduling_moderation_category(scheduling)
-    upsert_scheduling_moderation(scheduling.website, category)
+    category, moderation_validated = get_scheduling_moderation_category(scheduling)
+    upsert_scheduling_moderation(scheduling.website, category, moderation_validated)
