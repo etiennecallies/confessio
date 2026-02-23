a24de031ad7bed8e4b44ed77258bf20817d5bbb4
Etienne Callies <etienne.callies@ouihelp.fr>
2026-02-23 18:56:14 +0100

    add discord_utils

---
+++ core/utils/discord_utils.py
@@ -0,0 +1,41 @@
+import json
+import os
+from enum import Enum
+
+import httpx
+
+
+class DiscordChanel(Enum):
+    PB_MODO_CATHO = "pb_modo_catho"
+    PB_MODO_TECHNIQUE = "pb_modo_technique"
+    ALERTE_TECHNIQUE = "alerte_technique"
+    ALERTE_CONFLIT = "alerte_conflit"
+    ALERTE_BUG = "alerte_bug"
+
+
+def send_discord_alert(message: str, channel: DiscordChanel):
+    discord_webhooks_json = os.getenv("DISCORD_WEBHOOKS_JSON")
+
+    try:
+        discord_webhooks_dict = json.loads(discord_webhooks_json)
+        webhook_url = discord_webhooks_dict[channel.value]
+    except (TypeError, json.JSONDecodeError, KeyError) as e:
+        print(f"Erreur lors du chargement des webhooks Discord : {e}")
+        return
+
+    data = {
+        "content": message
+    }
+    response = httpx.post(webhook_url, json=data)
+
+    if response.status_code != 204:
+        print(f"Erreur lors de l'envoi de l'alerte Discord : "
+              f"{response.status_code} - {response.text}")
+    else:
+        print("Alerte Discord envoyée avec succès.")
+
+
+if __name__ == '__main__':
+    from dotenv import load_dotenv
+    load_dotenv()
+    send_discord_alert("Test d'alerte Discord depuis Confessio.", DiscordChanel.ALERTE_BUG)
